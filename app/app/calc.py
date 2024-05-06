78c806c3dd43df7d1457ef45840a4f6874046e00
Harsh Lath <harshlath@ip-192-168-75-224.ap-south-1.compute.internal>
2024-05-06 14:03:13 +0530

    initial code push

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator functions
+"""
+
+def add(x,y):
+    """Add x and y and return result"""
+    return x+y
+
+def subtract(x,y):
+    """Add x and y and return result"""
+    return x-y
